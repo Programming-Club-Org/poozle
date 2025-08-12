660739bad5844587c2be3acfe04b70c66162221d
Avi Shah (git) <mailbox.avishah+git@gmail.com>
2025-08-12 19:41:33 +0530

    Added comments

--- libpz/include/core.cpp
+++ libpz/include/core.cpp
@@ -1,6 +1,7 @@
 #include <pz_std.hpp>
 
 PzStd::PzCore::PzCore() {
+    // will be friend classes, so can use private constructors
     PzBuffer buffer_obj = new PzBuffer();
     this->buffer = make_shared<PzBuffer>(buffer_obj);
     PzAnalysis analysis_obj = new PzAnalysis();
