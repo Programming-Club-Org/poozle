4d7eb2d0ae29e7e810d1151ce9df1de8b314ec2f
Sanskar <gundesanskar71@gmail.com>
2025-08-14 00:33:06 +0530

    Run clang format

--- libpz/include/pz_std.hpp
+++ libpz/include/pz_std.hpp
@@ -1,10 +1,10 @@
 #ifndef PZ_STD_HPP
 #define PZ_STD_HPP
 
+#include <pz_buffer.hpp>
 #include <pz_cxx_std.hpp>
 #include <pz_error.hpp>
 #include <pz_types.hpp>
-#include <pz_buffer.hpp>
 
 namespace PzStd {
 class PzCore;
@@ -12,8 +12,6 @@ class PzBuffer;
 }; // namespace PzStd
 
 using PzErr = PzError::PzErrorType;
-
-
 class PzStd::PzCore {
 private:
   PzErr pz_error = PzErr::PZ_NO_ERROR;
@@ -22,7 +20,5 @@ public:
   // core operations on the object
   // eg. pz_core_input_buffer(const PzStd::PzBuffer& new_buffer);
 };
-
-
 
 #endif PZ_STD_HPP
