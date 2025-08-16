dfdc293a8ebf1facc916b5256ee08ee41455a4df
Avi Shah (git) <mailbox.avishah+git@gmail.com>
2025-08-16 18:15:01 +0530

    PzCore public and private constructors, with buffer setting.

--- libpz/include/core.cpp
+++
@@ -1,9 +0,0 @@
-#include <pz_std.hpp>
-
-PzStd::PzCore::PzCore() {
-    // will be friend classes, so can use private constructors
-    PzBuffer buffer_obj = new PzBuffer();
-    this->buffer = make_shared<PzBuffer>(buffer_obj);
-    PzAnalysis analysis_obj = new PzAnalysis();
-    this->analysis = make_shared<PzAnalysis>(analysis_obj);
-}
